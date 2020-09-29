710010552ec883e0fd2253c360ad7df25449fc7f
Georgedev2 <nwobodogeorge98@gmail.com>
2020-09-29 02:56:26 -0700

    Updates

---
+++ precache-manifest.5ca4902e74c98ea192ae054d506e8cc9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0eacc1d015cd4154d4719491f51b7d",
+    "url": "/procredita-home-page/index.html"
+  },
+  {
+    "revision": "85a4e9c377de03e3e77c",
+    "url": "/procredita-home-page/static/css/main.df3b6364.chunk.css"
+  },
+  {
+    "revision": "37f8dd31ecca6362d93d",
+    "url": "/procredita-home-page/static/js/2.1bb523f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/procredita-home-page/static/js/2.1bb523f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a4e9c377de03e3e77c",
+    "url": "/procredita-home-page/static/js/main.9c6e1543.chunk.js"
+  },
+  {
+    "revision": "460e1210f619606adc90",
+    "url": "/procredita-home-page/static/js/runtime-main.874bb3c4.js"
+  },
+  {
+    "revision": "490c0f04bf7e45f43f2c089c28bde014",
+    "url": "/procredita-home-page/static/media/client-four.490c0f04.svg"
+  },
+  {
+    "revision": "3f71b912f7d5f676cf78552324d41289",
+    "url": "/procredita-home-page/static/media/client-one.3f71b912.svg"
+  },
+  {
+    "revision": "4644dab3abf0d540062122755927c48a",
+    "url": "/procredita-home-page/static/media/client-three.4644dab3.svg"
+  },
+  {
+    "revision": "0d8147ccb462e990d8282c20bf32be8f",
+    "url": "/procredita-home-page/static/media/client-two.0d8147cc.svg"
+  },
+  {
+    "revision": "bb165e0f72f3016d386094935cfee12a",
+    "url": "/procredita-home-page/static/media/footer-logo.bb165e0f.svg"
+  },
+  {
+    "revision": "27e9405ccdd6225bdf208731ce75c5bc",
+    "url": "/procredita-home-page/static/media/header-logo.27e9405c.svg"
+  },
+  {
+    "revision": "0a1e889c6bfae88d0e71ab5dcf028e3b",
+    "url": "/procredita-home-page/static/media/our-tech-one.0a1e889c.svg"
+  },
+  {
+    "revision": "55fb8f89417187d5e642b53f24c47f3d",
+    "url": "/procredita-home-page/static/media/our-tech-three.55fb8f89.svg"
+  },
+  {
+    "revision": "55fb8f89417187d5e642b53f24c47f3d",
+    "url": "/procredita-home-page/static/media/our-tech-two.55fb8f89.svg"
+  },
+  {
+    "revision": "3f4e56e9dffc14b25ab66baa7e4eb056",
+    "url": "/procredita-home-page/static/media/regiona-franchise.3f4e56e9.svg"
+  },
+  {
+    "revision": "10cc98e8f207ef3f06451e8023966658",
+    "url": "/procredita-home-page/static/media/service-image.10cc98e8.svg"
+  },
+  {
+    "revision": "ebfb4586c74db896ed35670e3faaa7cd",
+    "url": "/procredita-home-page/static/media/what-we-do-btn.ebfb4586.svg"
+  },
+  {
+    "revision": "6dc1bbcf1ebaa159d8db7d2b021879ae",
+    "url": "/procredita-home-page/static/media/why-procredita (2).6dc1bbcf.svg"
+  },
+  {
+    "revision": "56cb311d2234c3fa4ccca4ab700d266c",
+    "url": "/procredita-home-page/static/media/why-procredita-logo.56cb311d.svg"
+  }
+]);
